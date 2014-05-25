bacf6d1fee3e3f624a1fdaafdffe8a4db1e3f63f
legoprog <legoprog@localhost.localdomain>
2014-05-25 10:10:50 -0400

    Some fix

--- bin/rtest.js
+++ bin/rtest.js
@@ -16,9 +16,33 @@ config = {
 var redis = new Redis(rediscnf);
 this.redis = redis;
 
-this.redis.getCats(function(res){
-    redis.getIpByCats(res, testip, function(match) {
-        console.log(match);
+redis.getCats(function(res){
+   redis.getIpByCats(res, testip, function(match) {
+
+       if(match != false){
+           var ipGroup = match;
+       }
+       else{
+           ipGroup = 'default';
+       }
+
+       redis.searchRulebyCat(ipGroup, function(ruleGroup) {
+           //console.log(ruleGroup[0]);
+           if(ruleGroup[0] === 1){
+               redis.GetRulebyCat(ipGroup, function(res) {
+                   console.log(ipGroup + ' : '+ res);
+
+               });
+           }
+           else{
+               console.log('err');
+           }
+
+
+
+
+       });
+
 
     });
 });
